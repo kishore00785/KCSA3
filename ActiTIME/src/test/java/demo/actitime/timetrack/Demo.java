3e8101acc01fca83a527f34d6e4be002ebeccce0
maste <maste@Kishore-Kumar>
2020-08-06 16:25:50 +0530

    My Project

---
+++ ActiTIME/src/test/java/demo/actitime/timetrack/Demo.java
@@ -0,0 +1,15 @@
+package demo.actitime.timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void runTest()
+	{
+		Reporter.log("Successfully Running",true);
+		
+	}
+	
+}
